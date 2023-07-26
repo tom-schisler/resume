78953710ee6f71425cea4ba2c1e3a5142ca69a34
tom-schisler <tschisler8579@gmail.com>
2023-07-26 14:54:44 -0400

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
 jQuery(function($) {
-  console.log('%c Breathe In, Breathe Out, Move On', 'font-weight: bold; font-size: 15px;color: #fff; text-shadow: 3px 3px 0 rgb(128, 53, 0) , 6px 6px 0 rgb(0, 32, 128)');
+  console.log('%c Breathe In, Breathe Out, Move On', 'font-weight: bold; font-size: 16px;color: #fff; text-shadow: 3px 3px 0 rgb(128, 53, 0) , 6px 6px 0 rgb(0, 32, 128)');
 });
