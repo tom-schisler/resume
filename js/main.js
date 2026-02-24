9157386c92af3e7d142bfbe760be3c694e8045b5
tom-schisler <tschisler8579@gmail.com>
2026-02-24 13:30:07 -0500

    Add screen shots for examples page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 jQuery(function($) {
+  // console message 
   console.log('%c Breathe In, Breathe Out, Move On', 'font-weight: bold; font-size: 16px;color: #fff; text-shadow: 3px 3px 0 rgb(128, 53, 0) , 6px 6px 0 rgb(0, 32, 128)');
+
+  // slider full size image
+  $('.carousel button').on('click', function() {
+    // "this" refers to the specific image element that was clicked
+    var imageUrl = $(this).find('img').attr('src'); 
+    var imageAlt = $(this).find('img').attr('alt'); 
+    
+    // Log the URL to the console
+    console.log(imageUrl); 
+    
+    $('#dialog figure img').attr('src', imageUrl).attr('alt', imageAlt);
+
+  });
+
 });
+
+//      <dialog id="dialog">
+//        <button commandfor="dialog" command="close">&times</button>
+//        <figure>
+//          <img src="img/portfolio/adeccousa-living-wage-tool-mobile.png" alt="" />
+//        </figure>
+//      </dialog>
